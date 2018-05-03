a78a00e22814bb1dac7526ae96d09cda79fe7094
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-05-03 16:28:42 -0400

    updated creature.py to match current version

--- halcyon/creature.py
+++ halcyon/creature.py
@@ -1,6 +1,7 @@
 class Creature():
 
     def __init__(self, name, on_planet, in_octant, health=5, move=0, player=0):
+        self.tags = {'Material' : [], 'Structure' : [], 'Function' : []}
         self.name = name
         self.health = health
         self.move = move
@@ -23,18 +24,24 @@ class Laborer(Creature):
         return 'a Laborer named %s' % self.name
 
     def harvest_resource(self, resource):
-        octant = self.octant
-        if resource in octant.resources:
+        if resource in self.octant.resources:
             return '%s is now harvesting %s' % (self.name, resource)
         return 'there is no %s in %s' % (resource, self.octant)
+
+    def construct_building(self, building_plan):
+        if building_plan in self.octant.contents:
+            print('%s is now building %s' % (self.name, building_plan))
+        print('there is no unfinished %s in %s' % (building_plan, self.octant))
 
 class Crew_Member(Laborer):
 
     def __init__(self, name, on_planet, in_octant, role):
         super().__init__(name, on_planet, in_octant, harvest_rate=1/12)
         self.role = role
+        self.tags['Material'].append('Organic')
 
 class Automaton(Laborer):
 
     def __init__(self, name, on_planet, in_octant):
         super().__init__(name, on_planet, in_octant, harvest_rate=1/6, build_rate=1/3)
+        self.tags['Material'].append('Metal')
