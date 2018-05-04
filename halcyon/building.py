1c8b7d19393af2bbe774247c30e4f2226bec832a
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-05-04 01:13:23 -0400

    - added the get_stat function in tags.py

--- halcyon/building.py
+++ halcyon/building.py
@@ -1,5 +1,5 @@
 from planet import Planet
-from tags import add_tags
+from tags import add_tags, get_stat
 
 class Building_Plan():
 
@@ -8,13 +8,14 @@ class Building_Plan():
         self.player = player
         self.planet = on_planet
         self.octant = in_octant
-        #Add the building plan to the contents of the octant
+        #add the building plan to the contents of the octant
         self.octant.add_occupant(self)
         #initialize the default, blank values for each tag category
         self.tags = {'Material' : [], 'Structure' : [], 'Function' : []}
         #send the tag argument to the add_tags function
         add_tags(self, tags)
-        #initalize
+        #initalize the amount of UoW needed to complete
+        #self.work_needed =
 
     def __str__(self):
         return 'a building plan for a %s' % self.name
@@ -22,4 +23,4 @@ class Building_Plan():
 Arrakis = Planet('Arrakis', 5, 5)
 House = Building_Plan('House', Arrakis, Arrakis.octants['North'], ['Wood', 'Enclosed', 'Factory: Wood Crafts'])
 
-print(House.tags['Function'])
+print(get_stat(House, 'Work'))
--- halcyon/tags.py
+++ halcyon/tags.py
@@ -1,13 +1,13 @@
 MATERIAL_TAGS = {
-    'Wood' : {'Attributes' : ['Flammable', 'Organic'], 'Statistics' : [10, 1]}
+    'Wood' : {'Attributes' : ['Flammable', 'Organic'], 'Statistics' : {'Health' : 10, 'Work' : 1}}
                 }
 
 STRUCTURE_TAGS = {
-    'Enclosed' : {'Attributes' : ['Enclosed'], 'Statistics' : [3, 1]}
+    'Enclosed' : {'Attributes' : ['Enclosed'], 'Statistics' : {'Health' : 3, 'Work' : 1}}
 }
 
 FUNCTION_TAGS = {
-    'Factory: Wood Crafts' : {'Attributes' : ['Create Wood Craft'], 'Statistics' : [0, 3]}
+    'Factory: Wood Crafts' : {'Attributes' : ['Create Wood Craft'], 'Statistics' : {'Work' : 3}}
 }
 
 def add_tags(self, tags):
@@ -21,3 +21,21 @@ def add_tags(self, tags):
         else:
             print("Tag '%s' does not exist" % tag)
             continue
+
+def get_stat(self, stat):
+    total = 0
+    #for each material tag
+    for material in self.tags['Material']:
+        #get the value of the given stat for each tag
+        tag_stat = MATERIAL_TAGS[material]['Statistics'][stat]
+        #add it to the total
+        total += tag_stat
+    #repeat for structure tags
+    for structure in self.tags['Structure']:
+        tag_stat = STRUCTURE_TAGS[structure]['Statistics'][stat]
+        total += tag_stat
+    #repeat for function tags
+    for function in self.tags['Function']:
+        tag_stat = FUNCTION_TAGS[function]['Statistics'][stat]
+        total += tag_stat
+    return total
