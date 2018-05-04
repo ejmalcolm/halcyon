553ac5e54ad2e12a5c982ac3276c591c2721b772
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-05-04 00:37:16 -0400

    - created add_tags function to add tags to a given object

--- halcyon/building.py
+++ halcyon/building.py
@@ -4,6 +4,24 @@ MATERIAL_TAGS = {
     'Wood' : {'Attributes' : ['Flammable', 'Organic'], 'Statistics' : [10, 1]}
                 }
 
+STRUCTURE_TAGS = {
+    'Enclosed' : {'Attributes' : ['Enclosed'], 'Statistics' : [3, 1]}
+}
+
+FUNCTION_TAGS = {}
+
+def add_tags(self, tags):
+    for tag in tags:
+        if tag in MATERIAL_TAGS:
+            self.material.append(tag)
+        elif tag in STRUCTURE_TAGS:
+            self.structure.append(tag)
+        elif tag in FUNCTION_TAGS:
+            self.function.append(tag)
+        else:
+            print("Tag '%s' does not exist" % tag)
+            continue
+
 class Building_Plan():
 
     def __init__(self, name, on_planet, in_octant, tags, player=0):
@@ -15,20 +33,16 @@ class Building_Plan():
         self.octant.add_occupant(self)
         #initialize the default, blank values for each tag category
         self.tags = {'Material' : [], 'Structure' : [], 'Function' : []}
-        self.material = None
-        self.structure = None
-        self.function = None
+        self.material = []
+        self.structure = []
+        self.function = []
         #send the tag argument to the add_tags function
-        add_tags(tags)
+        add_tags(self, tags)
 
     def __str__(self):
         return 'a building plan for a %s' % self.name
 
-    def add_tags(self, tags):
-        for tag in tags:
-            if tags
-
 Arrakis = Planet('Arrakis', 5, 5)
-House = Building_Plan('House', Arrakis, Arrakis.octants['North'], ['Wood', 'Enclosed'])
+House = Building_Plan('House', Arrakis, Arrakis.octants['North'], ['Wood', 'Enclosed', 'fuck'])
 
 #print(House.material)
