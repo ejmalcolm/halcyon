f655e4da38dfb89db1a00b43e98b7b0ef07ff121
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-05-06 02:50:19 -0400

    implemented the check_progress function in task.py

--- halcyon/task.py
+++ halcyon/task.py
@@ -1,10 +1,38 @@
-ACTIVE_TASKS = []
+from time import time, sleep
 
 class Task():
-    
-    def __init__(self, time_needed, end_function):
-        self.time_left = time_needed
-        self.end_func = end_function
-        
-    def decrease_time(self, amount):
-        self.time_left -= amount
+
+    def __init__(self, hours_needed, end_func):
+        #calculates the end_time and saves it as an attribute
+        #time() gets a relative number of seconds since the epoch
+        #then, the number of hours (time_needed) is converted to seconds
+        #the end time is set to the current time + the time needed
+        self.end_time = time() + (hours_needed*3600)
+        self.end_func = end_func
+
+    def check_progress(self):
+        print('called')
+        #if the end time has passed, call end_func())
+        if self.end_time <= time():
+            print('The task was finished.')
+            self.end_func()
+            return True
+        #otherwise, report the hours left
+        seconds_left = self.end_time - time()
+        if seconds_left >= 3600:
+            hours_left = int((seconds_left/3600))
+            print('there are %s hours remaining' % hours_left)
+            return False
+        else:
+            minutes_left = int((seconds_left/60))
+            print('there are %s minutes left' % minutes_left)
+        return False
+
+#this is the loop to check and update a task every 5 minutes
+while True:
+    try:
+        #replace task with the reference of the Task() to check
+        task.check_progress()
+        sleep(300)
+    except KeyboardInterrupt:
+        raise
