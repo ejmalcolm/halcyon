7edb484323bc65a61633ea55fe0583e8d6b244b2
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-05-16 15:10:04 -0400

    fuck guis

--- halcyon/halcyon_client.py
+++ halcyon/halcyon_client.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from task import ACTIVE_TASKS, Task
 
 def make_display_list(list):
@@ -10,3 +11,32 @@ def make_display_list(list):
         else:
             temp_text += str(item)
     return temp_text
+
+class HalcyonClient(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.frame = None
+        self.switch_frame(LoginPage)
+
+    def switch_frame(self, new_frame_class):
+        new_frame = new_frame_class(self)
+        if self.frame is not None:
+            self.frame.destroy()
+        self.frame = new_frame
+        self.frame.pack()
+
+class LoginPage(tk.Frame):
+
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        loginlabel = tk.Label(self, text='Enter your player name.')
+        loginlabel.pack()
+        loginentry = tk.Entry(self)
+        loginentry.pack()
+        loginbutton = tk.Button(self, text='Login')
+        loginbutton.pack()
+
+a = HalcyonClient()
+
+a.mainloop()
