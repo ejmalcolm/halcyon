a408227c03c5059f501b102f62583603523dac21
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-05-17 00:56:44 -0400

    added DetailView(), started work on events

--- halcyon/halcyon_client.py
+++ halcyon/halcyon_client.py
@@ -7,25 +7,43 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from planet import Planet
+
+PLANETS = { 'Dune' : Planet('Dune', 1, 2)}
+
+class DetailView(QtWidgets.QListWidget):
+    '''The class for displaying lists of class objects
+    Includes context menus for each class object displayed
+    '''
+
+    def __init__(self, parent, class_dict, player):
+        super().__init__(parent)
+        self.class_dict = class_dict
+        self.player = player
+        self.itemClicked.connect(self.Clicked)
+
+    def Clicked(self, item):
+        item_text = item.text()
+        print(self.class_dict[item_text])
 
 class Ui_Halcyon(object):
+
     def setupUi(self, Halcyon):
         Halcyon.setObjectName("Halcyon")
         Halcyon.resize(1200, 745)
         self.centralwidget = QtWidgets.QWidget(Halcyon)
         self.centralwidget.setObjectName("centralwidget")
-        #define planetview
-        self.PlanetView = QtWidgets.QListWidget(self.centralwidget)
+        ##define planetview
+        self.PlanetView = DetailView(self.centralwidget, PLANETS, 2)
         self.PlanetView.setGeometry(QtCore.QRect(10, 10, 225, 550))
         self.PlanetView.setToolTip("")
         self.PlanetView.setObjectName("PlanetView")
-        self.PlanetView.addItem('y')
-        #define playerview
+        ##define playerview
         self.PlayerView = QtWidgets.QListWidget(self.centralwidget)
         self.PlayerView.setGeometry(QtCore.QRect(965, 10, 225, 550))
         self.PlayerView.setToolTip("")
         self.PlayerView.setObjectName("PlayerView")
-        #define the octant label that says 'OctantView'
+        ##define the octant label that says 'OctantView'
         self.OctantLabel = QtWidgets.QLabel(self.centralwidget)
         self.OctantLabel.setGeometry(QtCore.QRect(470, 0, 211, 31))
         self.OctantLabel.setStyleSheet("font: 24pt \"Sitka\";")
@@ -33,15 +51,15 @@ class Ui_Halcyon(object):
         self.OctantLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.OctantLabel.setIndent(-1)
         self.OctantLabel.setObjectName("OctantLabel")
-        #define the central large octant view
+        ##define the central large octant view
         self.OctantView = QtWidgets.QListWidget(self.centralwidget)
         self.OctantView.setGeometry(QtCore.QRect(250, 40, 701, 521))
         self.OctantView.setObjectName("OctantView")
-        #define the alert view for messages/alerts
+        ##define the alert view for messages/alerts
         self.AlertView = QtWidgets.QListWidget(self.centralwidget)
         self.AlertView.setGeometry(QtCore.QRect(10, 570, 581, 121))
         self.AlertView.setObjectName("AlertView")
-        #define the task queue that shows all tasks
+        ##define the task queue that shows all tasks
         self.TaskQueue = QtWidgets.QListWidget(self.centralwidget)
         self.TaskQueue.setGeometry(QtCore.QRect(610, 570, 581, 121))
         self.TaskQueue.setObjectName("TaskQueue")
@@ -67,12 +85,14 @@ class Ui_Halcyon(object):
         self.OctantLabel.setText(_translate("Halcyon", "Octant View"))
         self.menutewst.setTitle(_translate("Halcyon", "Menu"))
 
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Halcyon = QtWidgets.QMainWindow()
     ui = Ui_Halcyon()
     ui.setupUi(Halcyon)
+    ####add custom code between here####
+    ui.PlanetView.addItems(list(PLANETS.keys()))
+    ####add custome code above here####
     Halcyon.show()
     sys.exit(app.exec_())
