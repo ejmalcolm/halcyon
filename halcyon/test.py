78982d1e2b8fd810b7e0212d9e80fed9227e1f6d
Evan J. Malcolm <evan.malcolm7@gmail.com>
2018-06-05 19:38:14 -0400

    tried solving a bug thats crashing the client when moving octant

--- halcyon/test.py
+++ halcyon/test.py
@@ -11,23 +11,20 @@ import inspect
 from collections import namedtuple
 import datetime
 
-# Dune = planet.Planet('Arrakis', 5, 5)
-#
-# ##dont delete above this plzty
-#
-# bldng = building.Building('Wood Craft Factory', Dune, Dune.octants['North'], ['Refinery: Stone', 'Factory: Wood Crafts'], player.GM)
-#
-# spcshp = vehicle.SpaceShip('Halcyon', Dune, Dune.octants['North'], 100000)
-#
-# crtr = creature.Creature('a', Dune, Dune.octants['North'], 0, 1)
-#
-# labor = creature.Laborer('bob', Dune, Dune.octants['North'], build_rate=5, player=player.GM)
-#
-# bplan = building.Building_Plan('buh', Dune, Dune.octants['North'], ['Wood', 'Metal'], player.GM)
+Dune = planet.Planet('Arrakis')
 
-buh = {'a' : 'b'}
+##dont delete above this plzty
 
-if buh[1] == 'b':
-    print(1)
-else:
-    print(2)
+bldng = building.Building('Wood Craft Factory', Dune, Dune.octants['North'], ['Refinery: Stone', 'Factory: Wood Crafts'], player.GM)
+
+spcshp = vehicle.SpaceShip('Halcyon', Dune, Dune.octants['North'], 100000)
+
+crtr = creature.Creature('a', Dune, Dune.octants['North'], 0, 1)
+
+labor = creature.Laborer('bob', Dune, Dune.octants['North'], build_rate=5, player=player.GM)
+
+bplan = building.BuildingPlan('buh', Dune, Dune.octants['North'], ['Wood', 'Metal'], player.GM)
+
+print(crtr.octant)
+crtr.move_octant('South')
+print(crtr.octant)
